e0f6fbab8004353a64f6af61006b0b53c5d45dc5
Gourav Verma <gouravv.contact@gmail.com>
2025-11-23 08:34:17 +0000

    initial steup

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "space-ui",
+      fileName: "space-ui",
+      formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDom",
+        },
+      },
+    },
+  },
+});
